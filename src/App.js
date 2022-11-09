cfd279feb1ad004a23c07ac0c422f667d2f92afe
Albert <aellrpeo@gmail.com>
2022-11-09 09:01:52 -0800

    Input and Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    setTodos([...todos, input])
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Todo App</h1>
+      <input value={input} onChange={event => setInput(event.target.value)}/>
+      <button onClick={addTodo}>Add Todo</button>
+
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
     </div>
   );
 }
