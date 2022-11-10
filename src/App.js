228a080592aaef5c7206a58bd2268c4c7f6c2983
Albert <aellrpeo@gmail.com>
2022-11-10 12:06:23 -0800

    Added Delete Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import './App.css';
 
+// DELETE - use filter function and id
+// EDIT BUTTON
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
@@ -9,16 +12,22 @@ function App() {
     setTodos([...todos, input])
     setInput('');
   }
-
+  const deleteTodo = (todoDelete) => () => {
+    setTodos(todos.filter(todo => todo !== todoDelete));
+  }
   return (
     <div className="App">
       <h1>Todo App</h1>
+      <div className='Todo'>
       <input value={input} onChange={event => setInput(event.target.value)}/>
       <button onClick={addTodo}>Add Todo</button>
 
         {todos.map(todo => (
-          <li>{todo}</li>
+          <div className='Todo__todoItem'><li>{todo}</li>
+          <button onClick={deleteTodo(todo)}>DELETE</button></div>
         ))}
+      </div>
+      
     </div>
   );
 }
